fe8ad0a71d363b0e8ec953fa6367457b94b0e870
Koulie <kouliekyriakos@hotmail.com>
2024-06-15 13:59:09 -0400

    Tested adding image to the site and sizing it with js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './style.css';
-
+import mainImage from './images/dubai-image.jpg';
 
 console.log("This is a test");
+const centerMainDiv = document.querySelector('.center-section');
+const myImage = new Image();
+myImage.classList.add('main-image');
+myImage.src = mainImage;
+centerMainDiv.appendChild(myImage);
+
+const imageElement = document.querySelector('.main-image');
+imageElement.src = Image;
