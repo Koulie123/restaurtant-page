2e8572c184300a0548685bacba9675a816d41451
Koulie <kouliekyriakos@hotmail.com>
2024-06-15 14:22:34 -0400

    changed image names

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import mainImage from './images/dubai-image.jpg';
 
 console.log("This is a test");
 const centerMainDiv = document.querySelector('.center-section');
-const myImage = new Image();
-myImage.classList.add('main-image');
-myImage.src = mainImage;
-centerMainDiv.appendChild(myImage);
-
-const imageElement = document.querySelector('.main-image');
-imageElement.src = Image;
+const centerImage = new Image();
+centerImage.classList.add('main-image');
+centerImage.src = mainImage;
+centerMainDiv.appendChild(centerImage);
